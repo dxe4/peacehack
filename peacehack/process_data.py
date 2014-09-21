0ffe9d042d57c18972ae7b5385b8c1ea4d7b3791
charalampos papaloizou <papaloizouc@gmail.com>
2014-09-21 06:59:53 +0100

    :gun:

--- peacehack/process_data.py
+++ peacehack/process_data.py
@@ -5,24 +5,41 @@ import matplotlib.pyplot as plt
 from theapp.models import CrazyObject
 
 
-def ukraine():
-    result = []
+def _get_data(year, month, day, country):
+    result_a = CrazyObject.objects.filter(
+        Year=year, Month=month, Day=day, Actor1CountryCode=country) \
+        .aggregate(Count('NumMentions'))
+
+    result_b = CrazyObject.objects.filter(
+        Year=year, Month=month, Day=day, Actor2CountryCode=country) \
+        .aggregate(Count('NumMentions'))
+    return result_a['NumMentions__count'] + result_b['NumMentions__count']
+
+
+def ukraine_february():
     days = list(range(1, 29))  # 2->3 reasons
-
-    for i in days:
-        result_a = CrazyObject.objects.filter(
-            Year=2014, Month=2, Day=i, Actor2CountryCode='UKR') \
-            .aggregate(Count('NumMentions'))
-
-        result_b = CrazyObject.objects.filter(
-            Year=2014, Month=2, Day=i, Actor1CountryCode='UKR') \
-            .aggregate(Count('NumMentions'))
-
-        _sum = result_a['NumMentions__count'] + result_b['NumMentions__count']
-        result.append(_sum)
+    result = [_get_data(2014, 2, day, 'UKR') for day in days]
 
     plt.plot(days, result)
     plt.ylabel('Times Mentioned')
     plt.xlabel('Day')
     plt.title('Ukraine times mentioned in febfruary')
     plt.savefig('ukraine.png')
+    plt.close()
+
+
+def ukraine_september():
+    days_aug = list(range(15, 30))  # 2->3 reasons
+    days_sep = list(range(1, 15))
+    days = days_sep + days_aug
+
+    result_sep = [_get_data(2014, 9, day, 'UKR') for day in days_sep]
+    result_aug = [_get_data(2014, 8, day, 'UKR') for day in days_aug]
+    result = result_aug + result_sep
+
+    plt.plot(days, result)
+    plt.ylabel('Times Mentioned')
+    plt.xlabel('Day')
+    plt.title('Ukraine times mentioned in august and september 2014')
+    plt.savefig('ukraine_aug_sept.png')
+    plt.close()
